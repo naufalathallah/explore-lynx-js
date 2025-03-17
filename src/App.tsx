793fd8f5288df9c1b9e72cddf340ef8179ae3ac7
Naufal A <spealdm6@gmail.com>
2025-03-17 20:38:58 +0700

    wip: init container

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
   }, []);
 
   return (
-    <view>
+    <view className="container">
       <text className="Title">React</text>
       <text className="Subtitle">on Lynx</text>
       <text>Check out the organized project structure!</text>
